e0821dc30b14c9537ff6d24723fd47724bc0eb9d
Yahav Hadad <yahav090@gmail.com>
2026-03-26 16:16:10 +0200

    use local state since public s3 bucket access is blocked

--- terraform/envs/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "async-msg-proc-tfstate-<ACCOUNT_ID>"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "async-msg-proc-tfstate-locks"
-    encrypt        = true
-  }
-}
